07e5d0f720c095ca7afe9fe0cc39583b387e9675
Anurag pandey <anuragpandey8056@gmail.com>
2024-09-30 11:40:15 +0530

    add prgram even odd

--- loops.py
+++ loops.py
@@ -46,20 +46,148 @@
 #                 print(x)
 #     x=x+1
 
-# ===================n==================================================
-y=int(input("enter a no"))
-x=1
+# ========================sum of upto even no================================================
+# y=int(input("enter a no"))
+# x=1
+# z=0
 
-while y*2>=x:
-    if x%2==0:
-        if x<y*2-1:
-            print(x,end=',')
-        else:
-            print(x)
+
+# while y>=x:
+
+#     if y%2==0:                 #to check value given even or odd
+#         if x%2==0:
+#             z=z+x             # to find even no
+#             if x<y:            # for output
+#                  print(x,end='+')  # to get 
+#             else:
+#                 print(x,end='=')
+#                 print(z)
+
+                
+
+#     else:
+#         if x%2==0:
+#             z=z+x             # to find even no
+
+#             if x<y-1:
+#                 print(x,end=("+"))
+#             else:
+#                 print(x,end='=')
+#                 print(z)
+#     x=x+1
+
+# ===================n even no==================================================
+# y=int(input("enter a no"))
+# x=1
+
+
+# while y*2>=x:
+#     if x%2==0:
+#         z=z+x
+#         if x<y*2-1:
+#             print(x,end='+')
+#         else:
+#             print(x)
 
         
-    x=x+1
+#     x=x+1
 
+# ===========================sum of n even no=====================================
+# y=int(input("enter a no"))
+# x=1
+# z=0
+
+# while y*2>=x:
+#     if x%2==0:
+#         z=z+x
+#         if x<y*2-1:
+#             print(x,end='+')
+#         else:
+#             print(x,end='=')
+#             print(z)
+
+        
+#     x=x+1
+
+
+# ===================================== generate upto odd no===================================
+# y=int(input("enter a no"))
+# x=1
+
+# while y>=x:
+#     if y%2==0:                 #to check value given even or odd
+#         if x%2!=0:             # to find even no
+#             if x<y-1:            # for output
+#                  print(x,end=',')  # to get 
+#             else:
+#                 print(x)
+
+#     else:
+#         if x%2!=0:
+#             if x<y-1:
+#                 print(x,end=(","))
+#             else:
+#                 print(x)
+#     x=x+1
+
+# ===========================sum of upto odd  no==============================================
+# y=int(input("enter a no"))
+# x=1
+# z=0
+
+# while y>=x:
+#     if y%2==0:                 #to check value given even or odd
+#         if x%2!=0:
+#             z=z+x             # to find even no
+#             if x<y-1:            # for output
+#                  print(x,end=',')  # to get 
+#             else:
+#                 print(x,end='=')
+#                 print(z)
+
+#     else:
+#         if x%2!=0:
+#             z=z+x
+#             if x<y-1:
+#                 print(x,end=(","))
+#             else:
+#                 print(x,end='=')
+#                 print(z)
+#     x=x+1
+
+
+# ===================n odd no==================================================
+# y=int(input("enter a no"))
+# x=1
+
+
+# while y*2>=x:
+#     if x%2!=0:
+#         if x<y*2-1:
+#             print(x,end='+')
+#         else:
+#             print(x)
+
+        
+#     x=x+1
+
+# =================== sum n odd no==================================================
+# y=int(input("enter a no"))
+# x=1
+# z=0
+
+
+# while y*2>=x:
+#     if x%2!=0:
+#         z=z+x
+#         if x<y*2-1:
+#             print(x,end='+')
+#         else:
+#             print(x,end='=')
+#             print(z)
+
+        
+#     x=x+1
 
 
     
