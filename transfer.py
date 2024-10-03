1055042eaaba3e3790024086bf6715dd7f583353
Anurag pandey <anuragpandey8056@gmail.com>
2024-10-03 20:09:38 +0530

    pattern added

--- transfer.py
+++ transfer.py
@@ -1,7 +1,7 @@
 # continue=> it is used to skip
 #break
 # pass
-# ==============================to escape continue =============================================
+# ==============================to escape continue=================== ======================================================================
 # n=int(input("enter a no"))
 # i=1
 # while i<=n:
@@ -11,15 +11,34 @@
 #     else:
 #         print(i)
 #     i=i+1
+# print("bye-bye")
 
-# ==============================to terminate loop break =============================================
-n=int(input("enter a no"))
-i=1
-while i<=n:
-    if i==5:
-        break
-    else:
-        print(i)
-    i=i+1
-print("bye-bye")
-print("h")
+
+
+# # ==============================to terminate loop break==================================================================================
+# n=int(input("enter a no"))
+# i=1
+# while i<=n:
+#     if i==5:
+#         break
+#     else:
+#         print(i)
+#     i=i+1
+# print("bye-bye")
+
+
+
+# ==============================pass ==================================================================================================
+# n=int(input("enter a no"))
+# i=1
+# while i<=n:
+#     if i==5:
+#         pass
+#     else:
+#         print(i)
+#     i=i+1
+# print("bye-bye")
+# =======================CONTINUE AND PASS DIFFERNCE=============================================
+# continue apne niche ke poore code ko skip karta hai aur break apnne loop se hi bahar ho jate hai 
+
+
