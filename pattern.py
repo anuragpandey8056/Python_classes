e01c0cd8701332b70584b8079670b80190e685c2
Anurag pandey <anuragpandey8056@gmail.com>
2024-10-03 22:05:45 +0530

    change

--- pattern.py
+++ pattern.py
@@ -1,14 +1,18 @@
 # ==================================================pattern==========================================================
-n=int(input("enter no of rowa"))
+# n=int(input("enter no of rowa"))
 
-for i in range(n):
-    print("*"*n)
+# for i in range(n):       # only last value show karega
+#     print("*"*n)
 
 # ============================================================
-# for i in range(1,6):                  
+# n=int(input("enter no of rowa"))
+
+# for i in range(1,n+1):                  
 #     print("*"*i)
 # ==========================================================
-# for i in range(1,6):
+# n=int(input("enter no of rowa"))
+
+# for i in range(1,n+1):
 #     print(' '*(5-i)+"*"*i)
 # ===========================================================
 # for i in range(1,6):
@@ -29,10 +33,25 @@ for i in range(n):
 
 # for i in range(n,0,-1):
 #     print(' '*(n-i)+" *"*i)
+# =======================================================================
+# n=int(input("enter no of rowa"))
+# for i in range(1,n+1):                  
+#     print("*"*i)
+# for i in range(n-1,0,-1):
+#     print('*'*i)
+
+# ==================================================================
+# n=int(input("enter no of rowa"))
+# for i in range(1,n+1):
+#     print(' '*(n-i)+"*"*i)
+# for i in range(n-1,0,-1):
+#     print(" "*(n-i)+'*'*i)
+
 
 # ===============================================
-# n=int(input("enter no of rowa"))
-# for i in range(1,n):
-#     print(' '*(5-i)+" *"*i)
-# for i in range(n,0,-1):
-#     print(' '*(n-i)+" *"*i)
+# n= int(input("enter a no"))
+# for i in range(1,n+1):
+#     print("*"*i)
+# for i in range(n-1,0,-1):
+#     print(" "*(n-i)+"*"*i)
+
